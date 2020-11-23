69848dd18287e1d25d9ae160772deee9ee8c7d03
hiro0118 <fuji.hiroshi0118@gmail.com>
2020-11-22 23:13:29 -0700

    improve toString in TaskDate

--- TaskService/src/application/TaskDate.java
+++ TaskService/src/application/TaskDate.java
@@ -26,8 +26,7 @@ public class TaskDate {
 
 	@Override
 	public String toString() {
-		String builder = getClass().getSimpleName() + "=" + DateString;
-		return builder;
+		return getClass().getSimpleName() + "=" + DateString;
 	}
 
 	@Override
