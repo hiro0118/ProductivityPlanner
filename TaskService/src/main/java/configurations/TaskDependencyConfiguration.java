01d277ee2859015c26cb40238b295482d5e503f5
hiro0118 <fuji.hiroshi0118@gmail.com>
2020-12-21 21:40:27 -0700

    improve config and db script

--- TaskService/src/main/java/configurations/TaskDependencyConfiguration.java
+++ TaskService/src/main/java/configurations/TaskDependencyConfiguration.java
@@ -1,5 +1,7 @@
 package configurations;
 
+import domain.ITaskRepository;
+import domain.ITaskService;
 import domain.TaskService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +15,14 @@ public class TaskDependencyConfiguration {
 	private static final String PROPERTY_PATH = "./TaskService/target/classes/task_service.properties";
 
 	@Bean
-	public MySqlTaskRepository mySqlTaskRepository() {
+	public ITaskRepository mySqlTaskRepository() {
 		TaskProperties property = new TaskProperties(PROPERTY_PATH);
 		IMysqlConfiguration config = new MysqlConfiguration(property);
 		return new MySqlTaskRepository(config);
 	}
 
 	@Bean
-	public TaskService taskService() {
+	public ITaskService taskService() {
 		return new TaskService(mySqlTaskRepository());
 	}
 }
