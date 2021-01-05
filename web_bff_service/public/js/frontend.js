2b3ec03a26efbe354ed192bbaef7adaabadb3bbf
hiro0118 <fuji.hiroshi0118@gmail.com>
2021-01-04 17:59:20 -0700

    make task a component

--- web_bff_service/public/js/frontend.js
+++ web_bff_service/public/js/frontend.js
@@ -1,8 +1,14 @@
 function getTasks() {
+    change();
     let request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:49160/tasks/');
     request.onload = function () {
         console.log(this.response);
     }
     request.send(null);
+}
+
+function change() {
+    var elem = document.getElementById("card1");
+    elem.innerHTML = "<h5 class='card-title' id='card1'>Task title2</h5>";
 }
